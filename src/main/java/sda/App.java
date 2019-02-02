749f73b63ac60c6c477c21ca919d44c634a9b6c7
AnitaMalek <anitamalek1991@gmail.com>
2019-02-02 12:35:17 +0100

    *

---
+++ src/main/java/sda/App.java
@@ -0,0 +1,26 @@
+package sda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+
+        int i = 0;
+        nieparzysta(6);
+
+    }
+
+
+    public static void nieparzysta (int i) {
+        if (i % 2 == 1) {
+            System.out.println("liczba nieparzysta");;}
+        else{
+            System.out.println("liczba parzysta");}
+
+    }
+
+
+}
+
